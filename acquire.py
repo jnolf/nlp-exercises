80a9962e42b917e05b81985e4a080fdc05fdae51
Jerry Nolf <nolf.jerry@gmail.com>
2022-05-12 10:17:15 -0500

    updated

--- acquire.py
+++ acquire.py
@@ -2,6 +2,7 @@ import requests
 from requests import get
 from bs4 import BeautifulSoup
 import pandas as pd
+import os
 
 pd.set_option('display.max_columns', None)
 pd.set_option('display.max_rows', None)
@@ -9,31 +10,37 @@ pd.set_option('display.max_rows', None)
 
 # Acquire Codeup Blog Articles
 
-def get_front_page_links():
-    """
-    Short function to hit the codeup blog landing page and return a list of all the urls to further blog posts on the
-    page.
-    """
-    response = requests.get("https://codeup.com/blog/", headers={"user-agent": "Codeup Data Science"})
+def get_blog_article_urls():
+    headers = {'user-agent': 'Innis Codeup Data Science'}
+    response = requests.get('https://codeup.com/blog/', headers=headers)
     soup = BeautifulSoup(response.text)
-    links = [link.attrs["href"] for link in soup.select(".more-link")]
-    return links
+    urls = [a.attrs['href'] for a in soup.select('a.more-link')]
+    return urls
 
-def parse_codeup_blog_article(url):
-    "Given a blog article url, extract the relevant information and return it as a dictionary."
-    response = requests.get(url, headers={"user-agent": "Codeup Data Science"})
-    soup = BeautifulSoup(response.text)
+def parse_blog_article(soup):
     return {
-        "title": soup.select_one(".entry-title").text,
-        "published": soup.select_one(".published").text,
-        "content": soup.select_one(".entry-content").text.strip(),
+        'title': soup.select_one('h1.entry-title').text,
+        'published': soup.select_one('.published').text,
+        'content': soup.select_one('.entry-content').text.strip(),
     }
 
-def get_blog_articles():
-    "Returns a dataframe where each row is a blog post from the codeup blog landing page."
-    links = get_front_page_links()
-    df = pd.DataFrame([parse_codeup_blog_article(link) for link in links])
-    return df
+def get_codeup_articles(use_cache=True):
+    if os.path.exists('codeup_blog_articles.json') and use_cache:
+        return pd.read_json('codeup_blog_articles.json')
+
+    urls = get_blog_article_urls()
+    headers = {'user-agent': 'Innis Codeup Data Science'}
+    articles = []
+
+    for url in urls:
+        print(f'fetching {url}')
+        response = requests.get(url, headers=headers)
+        soup = BeautifulSoup(response.text)
+        articles.append(parse_blog_article(soup))
+
+    df = pd.DataFrame(articles)
+    df.to_json('codeup_blog_articles.json', orient='records')
+    return 
 
 
 # Acquire InShorts Articles
@@ -42,10 +49,11 @@ def parse_news_card(card, category):
     '''
     Take in a news card and generate a dictionary
     '''
-    cards = soup.select_one('div.news-card')
     output = {}
     output['category'] = category
     output['title'] = card.select_one('a.clickable').text.strip()
+    
+    cards = card.select_one('div.news-card')
     output['content'] = card.select_one('div.news-card-content').text.strip()
     output['author'] = card.select_one('.author').text
     output['published'] = card.select_one('.time').attrs['content']
@@ -71,5 +79,7 @@ def get_inshorts_articles():
     for category in categories:
         print(f'Getting articles for {category}')
         category_articles = parse_inshorts_page(category)
-        articles.extend(category_articles)    
+        articles.extend(category_articles) 
+    df=pd.DataFrame(articles)
+    df.to_json('news_articles.json', orient='records')
     return df
